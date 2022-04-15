c618cf1fe0eca39ed4375320615f512442cebbbb
Alwinjesus <93728357+Alwinjesus@users.noreply.github.com>
2022-04-15 12:14:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,64 @@
-import telegram.ext
 
-with open('token.txt', 'r') as f:
-    TOKEN = f.read()
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+PORT = int(os.environ.get('PORT', 5000))
 
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+TOKEN = '5002337751:AAFxNJ1RJ8-cssdniJmA1LcLFQaULSr1QUc'
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
-    update.message.reply_text("Hi Welcome")
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-def handle_message(update, context):
-    if update.message.text == "bye":
-        update.message.reply_text(f"you said {update.message.text}")
-    elif update.message.text == "what is your name":
-        update.message.reply_text("I am Alwin")
-        update.message.make_as_read()
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
-updater = telegram.ext.Updater(TOKEN, use_context=True)
-disp = updater.dispatcher
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
-disp.add_handler(telegram.ext.CommandHandler("start", start))
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
 
-updater.start_polling()
-updater.idle()
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
